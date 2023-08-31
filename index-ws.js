916de94659f20f4336bf53bfc0a365399f6d4165
Diego Hernandez <diegofootball19@gmail.com>
2023-08-30 19:04:02 -0500

    testign

--- index-ws.js
+++ index-ws.js
@@ -1,21 +1,37 @@
-const express = require('express');
-const server = require('http').createServer();
+const express = require("express");
+const server = require("http").createServer();
 const app = express();
 const PORT = 3000;
-app.get('/', ( req, res ) => {
-    res.sendFile('index.html', {root: __dirname});
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
 });
 
-server.on('request', app);
-server.listen( 3000,  () => console.log(`server started on PORT ${PORT}`));
+server.on("request", app);
+server.listen(3000, () => console.log(`server started on PORT ${PORT}`));
 
 /** Begin websockets **/
 
-const WEBSOCKETSERVSER = require('ws').Server;
+const WEBSOCKETSERVSER = require("ws").Server;
 
-const wss = new WEBSOCKETSERVSER({server: server});
+const wss = new WEBSOCKETSERVSER({ server: server });
 
-wss.on('connection',  function connection(ws) {
-    const numClients = wss.clients.size;
-    console.log(`Clients conntect ${numClients}`);
-})
+wss.on("connection", function connection(ws) {
+  const numClients = wss.clients.size;
+  console.log(`Clients conntected ${numClients}`);
+
+  wss.broadcast(`Current visitors: ${numClients}`);
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send("Welcome to my server");
+  }
+  ws.on("close", function close() {
+    console.log("A client has disconnected"),
+      wss.broadcast(`Current visitors: ${numClients}`);
+  });
+});
+// broad
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
